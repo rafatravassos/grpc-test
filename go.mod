08491bdc5b2e0559b3219d8f73dce7c3e7f356e9
Rafael Travassos <barbeiro.java@gmail.com>
2023-03-02 10:44:49 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rafatravassos/grpc-test
+
+go 1.20
+
+require google.golang.org/protobuf v1.28.1
---
+++ internal/service/category.go
@@ -0,0 +1,11 @@
+package service
+
+type CategoryService struct{}
+
+func NewCategoryService() *CategoryService {
+	return &CategoryService{}
+}
+
+func main() {
+
+}
